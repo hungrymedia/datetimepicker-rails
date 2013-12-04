bb9a5588ea5193e12b373a502982195ce95a0bff
Warren Harrison <wharrison@topicdesign.com>
2013-12-04 09:51:05 -0500

    Update input templates to use glyphicons.

--- lib/generators/templates/inputs/time_picker_input.rb
+++ lib/generators/templates/inputs/time_picker_input.rb
@@ -13,7 +13,7 @@ class TimePickerInput < SimpleForm::Inputs::StringInput
     template.content_tag :div, class: 'input-append date timepicker' do
       input = super # leave StringInput do the real rendering
       input += template.content_tag :span, class: 'add-on' do
-        template.content_tag :i, '', class: 'icon-time', data: { 'time-icon' => 'icon-time', 'date-icon' => 'icon-calendar' }
+        template.content_tag :i, '', class: 'glyphicon glyphicon-time', data: { 'time-icon' => 'icon-time', 'date-icon' => 'icon-calendar' }
       end
       input
     end
